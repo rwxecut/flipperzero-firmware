f88ffac6333ade8ed776bfdf7103cc60ee202471
Konstantin Kompan <atomnik07@gmail.com>
2021-12-04 15:16:24 +0300

    we do a little refactor :nail_care:

--- applications/usb-ir-dongle/xecut_usb_ir_dongle.c
+++ applications/usb-ir-dongle/xecut_usb_ir_dongle.c
@@ -6,12 +6,10 @@
 
 
 typedef struct {
-	bool press[5];
-	uint16_t up;
-	uint16_t down;
-	uint16_t left;
-	uint16_t right;
-	uint16_t ok;
+	osMessageQueueId_t event_queue;
+	ViewPort* view_port;
+	Gui* gui;
+	osTimerId_t timer;
 } XecutUIDState;
 
 typedef enum {
@@ -27,22 +25,24 @@ typedef struct {
 } XecutUIDEvent;
 
 
-static void xecut_usb_ir_dongle_input_callback(InputEvent* input_event, osMessageQueueId_t event_queue) {
-	furi_assert(event_queue);
+static void xecut_usb_ir_dongle_input_callback(InputEvent* input_event, void* ctx) {
+	furi_assert(ctx);
+	XecutUIDState *state = (XecutUIDState*)ctx;
 	XecutUIDEvent event = {.type = EventTypeInput, .input = *input_event};
-	osMessageQueuePut(event_queue, &event, 0, osWaitForever);
+	osMessageQueuePut(state->event_queue, &event, 0, osWaitForever);
 }
 
 
-static void xecut_usb_ir_dongle_tick_callback(osMessageQueueId_t event_queue) {
-	furi_assert(event_queue);
+static void xecut_usb_ir_dongle_tick_callback(void* ctx) {
+	furi_assert(ctx);
+	XecutUIDState *state = (XecutUIDState*)ctx;
 	XecutUIDEvent event = {.type = EventTypeTick};
-	osMessageQueuePut(event_queue, &event, 0, 0);
+	osMessageQueuePut(state->event_queue, &event, 0, 0);
 }
 
 
 static void xecut_usb_ir_dongle_render_callback(Canvas* canvas, void* ctx) {
-	XecutUIDState* state = (XecutUIDState*)acquire_mutex((ValueMutex*)ctx, 25);
+	XecutUIDState *state = (XecutUIDState*)acquire_mutex((ValueMutex*)ctx, 25);
 	if (!state) return;
 
 	canvas_set_font(canvas, FontPrimary);
@@ -56,32 +56,44 @@ static void xecut_usb_ir_dongle_render_callback(Canvas* canvas, void* ctx) {
 }
 
 
+static XecutUIDState* xecut_usb_ir_dongle_init(ValueMutex* state_mutex) {
+	XecutUIDState* state = furi_alloc(sizeof(XecutUIDState));
+	state->event_queue = osMessageQueueNew(8, sizeof(XecutUIDEvent), NULL);
+	furi_check(state->event_queue);
+	state->view_port = view_port_alloc();
+	view_port_draw_callback_set(state->view_port, xecut_usb_ir_dongle_render_callback, state_mutex);
+	view_port_input_callback_set(state->view_port, xecut_usb_ir_dongle_input_callback, state);
+	state->gui = furi_record_open("gui");
+	gui_add_view_port(state->gui, state->view_port, GuiLayerFullscreen);
+	state->timer = osTimerNew(xecut_usb_ir_dongle_tick_callback, osTimerPeriodic, state, NULL);
+	osTimerStart(state->timer, osKernelGetTickFreq() / 4);
+	return state;
+}
+
+
+static void xecut_usb_ir_dongle_free(XecutUIDState* state) {
+	view_port_enabled_set(state->view_port, false);
+	gui_remove_view_port(state->gui, state->view_port);
+	view_port_free(state->view_port);
+	furi_record_close("gui");
+	osTimerDelete(state->timer);
+	osMessageQueueDelete(state->event_queue);
+	free(state);
+}
+
+
 int32_t xecut_usb_ir_dongle_app(void* p) {
-	osMessageQueueId_t event_queue = osMessageQueueNew(8, sizeof(XecutUIDState), NULL);
-	furi_check(event_queue);
-
-	XecutUIDState _state = {0};
-
 	ValueMutex state_mutex;
-	if(!init_mutex(&state_mutex, &_state, sizeof(XecutUIDState))) {
-		FURI_LOG_E(TAG, "cannot create mutex");
+	XecutUIDState *state = xecut_usb_ir_dongle_init(&state_mutex);
+	if (!init_mutex(&state_mutex, state, sizeof(XecutUIDState))) {
+		FURI_LOG_E(TAG, "failed to create mutex");
 		return 256;
 	}
 
-	ViewPort* view_port = view_port_alloc();
-	view_port_draw_callback_set(view_port, xecut_usb_ir_dongle_render_callback, &state_mutex);
-	view_port_input_callback_set(view_port, xecut_usb_ir_dongle_input_callback, event_queue);
-
-	Gui* gui = furi_record_open("gui");
-	gui_add_view_port(gui, view_port, GuiLayerFullscreen);
-
-	osTimerId_t timer = osTimerNew(xecut_usb_ir_dongle_tick_callback, osTimerPeriodic, event_queue, NULL);
-	osTimerStart(timer, osKernelGetTickFreq() / 4);
-
 	XecutUIDEvent event;
 	for (bool running = true; running;) {
-		osStatus_t event_status = osMessageQueueGet(event_queue, &event, NULL, 100);
-		XecutUIDState* state = (XecutUIDState*)acquire_mutex_block(&state_mutex);
+		osStatus_t event_status = osMessageQueueGet(state->event_queue, &event, NULL, 100);
+		XecutUIDState *state = (XecutUIDState*)acquire_mutex_block(&state_mutex);
 		if (event_status == osOK) {
 			if (event.type == EventTypeInput && event.input.type == InputTypePress) {
 				switch (event.input.key) {
@@ -94,16 +106,11 @@ int32_t xecut_usb_ir_dongle_app(void* p) {
 				// do smth
 			}
 		}
-		view_port_update(view_port);
+		view_port_update(state->view_port);
 		release_mutex(&state_mutex, state);
 	}
 
-	osTimerDelete(timer);
-	view_port_enabled_set(view_port, false);
-	gui_remove_view_port(gui, view_port);
-	view_port_free(view_port);
-	furi_record_close("gui");
-	osMessageQueueDelete(event_queue);
 	delete_mutex(&state_mutex);
+	xecut_usb_ir_dongle_free(state);
 	return 0;
 }
